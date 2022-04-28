6e36b1277a47f957c265e5bc87c0d7404eb47968
rodelcabil <rodeliocabil09@gmail.com>
2022-04-29 00:31:21 +0800

    patient details updated ui

--- screens/viewPatientDetails.js
+++ screens/viewPatientDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, StyleSheet, SafeAreaView, Text, Image, ScrollView, Button, useWindowDimensions } from 'react-native';
+import { View, StyleSheet, SafeAreaView, Text, Image, ScrollView, TouchableWithoutFeedback, useWindowDimensions } from 'react-native';
 import AppBar from './ReusableComponents/AppBar';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -9,18 +9,26 @@ import FAIcon5 from 'react-native-vector-icons/FontAwesome5';
 import FontistoIcon from 'react-native-vector-icons/Fontisto';
 import moment from 'moment';
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import {AccordionList} from "accordion-collapse-react-native";
+import { Separator } from 'native-base';
+import { List } from 'react-native-paper';
 
 
 const ViewPatientDetails = ({ route }) => {
 
     const [patientDetails, setPatientDetails] = useState([]);
+    // const [cases, setCases] = useState([{case_id: 1, notes: 'notes 1', type: 'type 1'}, {case_id: 2, notes: 'notes 2', type: 'type 2'}, {case_id: 3, notes: 'notes 3', type: 'type 3'}, {case_id: 4, notes: 'notes 4', type: 'type 4'} , {case_id: 5, notes: 'notes 5', type: 'type 5'} , {case_id: 6, notes: 'notes 6', type: 'type 6'}, {case_id: 7, notes: 'notes 7', type: 'type 7'}, {case_id: 9, notes: 'notes 9', type: 'type 8'}]);
+    const [selectedCase, setSelectedCase] = useState();
     const [cases, setCases] = useState([]);
-
     const [index, setIndex] = React.useState(0);
     const [routes] = React.useState([
-        { key: 'first', title: 'Other Information' },
+        { key: 'first', title: 'Information' },
         { key: 'second', title: 'Cases' },
     ]);
+
+    const [expanded, setExpanded] = React.useState(true);
+
+    const handlePress = () => setExpanded(!expanded);
 
     const layout = useWindowDimensions();
 
@@ -47,56 +55,116 @@ const ViewPatientDetails = ({ route }) => {
     }, []);
 
     const InformationRoute = () => (
-        <View style={styles.tabContainer}>
+        <View style={styles.container}>
             
             <ScrollView>
                 <View style={styles.body}>
-                    <View style={styles.detailsContainer}>
-                        <Icon name="email" size={20} color="#da7331" />
-                        <Text style={styles.textDetails}>{patientDetails.email_address}</Text>
-                    </View>
-                    <View style={styles.detailsContainer}>
-                        <EntypoIcon name="phone" size={20} color="#da7331" />
-                        <Text style={styles.textDetails}>{patientDetails.contact_number}</Text>
-                    </View>
-                    <View style={styles.detailsContainer}>
-                        <FAIcon5 name="birthday-cake" size={20} color="#da7331" />
-                        <Text style={styles.textDetails}>{moment(patientDetails.birthday).format('ll')}</Text>
-                    </View>
-                    <View style={styles.detailsContainer}>
-                        <Icon name="human-male-height" size={20} color="#da7331" />
-                        <Text style={styles.textDetails}>{patientDetails.height}</Text>
-                    </View>
-                    <View style={styles.detailsContainer}>
-                        <Icon name="weight" size={20} color="#da7331" />
-                        <Text style={styles.textDetails}>{patientDetails.weight}</Text>
-                    </View>
-                    <View style={styles.detailsContainer}>
-                        <EntypoIcon name="location-pin" size={23} color="#da7331" />
-                        <Text style={styles.textDetails}>{patientDetails.city}, {patientDetails.state}, {patientDetails.country}</Text>
-                    </View>
-    
-    
-    
-    
-                    {/* <View style={styles.historyWrapper}>
-                    <Text style={styles.textHistory}>History</Text>
-                    {cases?.map((data, index) => {
-                       return <View key={index} style={styles.historyContainer}>
-                                <Text style={styles.caseTitle}>{data.case_id}</Text>
-                              
-                              </View>
-                    })}
                     
-                </View> */}
+                    <View style={styles.detailsColumnContainer}>
+                        {/* <FAIcon5 name="birthday-cake" size={20} color="#da7331" /> */}
+                        <Text style={styles.textBigDetails}>BIRTHDAY</Text>
+                        <Text style={styles.textColDetails}>{moment(patientDetails.birthday).format('ll')}</Text>
+                    </View>
+                    <View style={styles.detailsColumnContainer}>
+                        {/* <Icon name="human-male-height" size={20} color="#da7331" /> */}
+                        <Text style={styles.textBigDetails}>HEIGHT</Text>
+                        <Text style={styles.textColDetails}>{patientDetails.height}</Text>
+                    </View>
+                    <View style={styles.detailsColumnContainer}>
+                        {/* <Icon name="weight" size={20} color="#da7331" /> */}
+                        <Text style={styles.textBigDetails}>WEIGHT</Text>
+                        <Text style={styles.textColDetails}>{patientDetails.weight}</Text>
+                    </View>
+                    <View style={styles.detailsColumnContainer}>
+                        {/* <EntypoIcon name="location-pin" size={23} color="#da7331" /> */}
+                        <Text style={styles.textBigDetails}>LOCATION</Text>
+                        <Text style={styles.textColDetails}>{patientDetails.city}, {patientDetails.state}, {patientDetails.country}</Text>
+                    </View>    
+                    <View style={styles.detailsColumnContainer}>
+                        {/* <EntypoIcon name="location-pin" size={23} color="#da7331" /> */}
+                        <Text style={styles.textBigDetails}>SURGEON</Text>
+                        <Text style={styles.textColDetails}>{patientDetails.surgeon}</Text>
+                    </View> 
+                    <View style={styles.detailsColumnContainer}>
+                        {/* <EntypoIcon name="location-pin" size={23} color="#da7331" /> */}
+                        <Text style={styles.textBigDetails}>ETHNICITY</Text>
+                        <Text style={styles.textColDetails}>{patientDetails.ethnicity}</Text>
+                    </View>  
+                    <View style={styles.detailsColumnContainer}>
+                        {/* <EntypoIcon name="location-pin" size={23} color="#da7331" /> */}
+                        <Text style={styles.textBigDetails}>PREFFERED METHOD</Text>
+                        <Text style={styles.textColDetails}>{patientDetails.preferred_method}</Text>
+                    </View>     
+                    <View style={styles.detailsColumnContainer}>
+                        {/* <EntypoIcon name="location-pin" size={23} color="#da7331" /> */}
+                        <Text style={styles.textBigDetails}>HOW DID YOU HEAR US?</Text>
+                        <Text style={styles.textColDetails}>{patientDetails.how_did_hear}</Text>
+                    </View>                    
+                    <View style={styles.detailsColumnContainer}>
+                        {/* <EntypoIcon name="location-pin" size={23} color="#da7331" /> */}
+                        <Text style={styles.textBigDetails}>COMMENTS</Text>
+                        <Text style={styles.textColDetails}>{patientDetails.comments}</Text>
+                    </View>
+                    <View style={styles.detailsColumnContainer}>
+                        {/* <EntypoIcon name="location-pin" size={23} color="#da7331" /> */}
+                        <Text style={styles.textBigDetails}>PAST SURGERY</Text>
+                        <Text style={styles.textColDetails}>{patientDetails.past_surgery}</Text>
+                    </View>
                 </View>
             </ScrollView>
         </View>
     );
+
+
+
+    const header = (item) =>{
+        return(
+            <View >
+                <Separator  style={{backgroundColor: '#da7331', height: 50, paddingHorizontal: 10, borderWidth: 0.6, borderColor:'#e3e3e3', marginTop: 2, borderRadius: 5}}>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                        <Text style={{color: '#fff', fontSize: 16, paddingVertical: 10}}>{item.case_id}</Text>
+                        {/* <EntypoIcon name={selectedCase !== item.case_id ? "chevron-down" : "chevron-up"}  size={23} color="#da7331" /> */}
+                    </View>
+                </Separator>
+            </View>
+           
+           
+        );
+    }
+
+    const accordionBody =(item)=>{
+        return (
+            <>
+                <View style={{paddingVertical:12, backgroundColor: '#fff', borderBottomWidth: 2, borderLeftWidth: 0.6, borderRightWidth: 0.6, borderColor:'#e3e3e3', marginTop: -2}}>
+                    <View style={{padding:10, backgroundColor: '#fff', borderWidth: 1, marginHorizontal: 10, marginBottom:10, borderRadius: 6, borderColor:'#e3e3e3'}}>
+                        <Text style={{color: 'black', fontSize: 16}}>{item.type}</Text>
+                        <Text style={{fontSize: 14, color: 'black'}}>{item.notes}</Text>
+                    </View>
+                    <View style={{padding:10, backgroundColor: '#fff', borderWidth: 1, marginHorizontal: 10, borderRadius: 6, borderColor:'#e3e3e3'}}>
+                        <Text style={{color: 'black', fontSize: 16}}>MESSAGE BOARD</Text>
+                       
+                    </View>
+                </View>
+                {/* <View style={{paddingVertical:12, backgroundColor: '#fff', borderBottomWidth: 2, borderLeftWidth: 0.6, borderRightWidth: 0.6, borderColor:'#e3e3e3', marginTop: -2}}>
+                   
+                </View> */}
+            </>
+        );
+    }
     
     const SecondRoute = () => (
-        <View style={styles.tabContainer} />
+        <View style={styles.casesContainer} >
+            <AccordionList
+                list={cases}
+                header={header}
+                body={accordionBody}
+                keyExtractor={item => `${item.case_id}`}
+                
+            />
+        </View>
     );
+
+    
 
     
     const renderScene = SceneMap({
@@ -158,11 +226,11 @@ const styles = StyleSheet.create({
         fontFamily: "Poppins",
 
     },
-    tabContainer: {
+    casesContainer: {
         flex: 1,
-        backgroundColor: '#F2F4F5',
+        backgroundColor: '#fff',
         fontFamily: "Poppins",
-
+        padding: 10
     },
     
     body: {
@@ -185,6 +253,9 @@ const styles = StyleSheet.create({
     },
     detailsColumnContainer: {
         flexDirection: 'column',
+        borderBottomWidth: 0.6,
+        borderColor: '#e2e2e2',
+        paddingVertical: 10,
     },
     textName: {
         fontSize: 22,
@@ -206,6 +277,18 @@ const styles = StyleSheet.create({
         color: '#000',
         fontSize: 16,
         fontWeight: '400'
+    },
+    textColDetails:{
+        
+        color: '#000',
+        fontSize: 18,
+        fontWeight: '400'
+    },
+    textBigDetails:{
+        color: '#737A87',
+        fontSize: 13,
+        fontWeight: '400',
+        marginBottom: 3
     },
     historyWrapper: {
         paddingTop: 20,
